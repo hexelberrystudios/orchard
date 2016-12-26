9c9067acf99124d8b36002c971fdf12d07cb4a09
Jonathan S. Collins León <yotsubato32@gmail.com>
2016-12-25 21:23:40 -0600

    Building proof of concept

---
+++ public/index.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Step 1: Require the Slim Framework
+ *
+ * If you are not using Composer, you need to require the
+ * Slim Framework and register its PSR-0 autoloader.
+ *
+ * If you are using Composer, you can skip this step.
+ */
+require '../vendor/autoload.php';
+require '../vendor/PHPMailer/PHPMailerAutoload.php';
+
+
+\Slim\Slim::registerAutoloader();
+/*
+function connect_db() {
+	$hostname = 'mysql.howdidigethere.info'; // this may be an ip address instead
+	$username = 'pernileon';
+	$password = 'crimsonknightscheeseportal';
+	$database = 'how_db';
+    
+    //$connection = mysql_connect($hostname,$username,$password);
+    //mysql_select_db($database) or die("Unable to select database");
+	$connection = new mysqli($hostname, $username, $password, $database);
+    
+	return $connection;
+}
+*/
+
+/**
+ * Step 2: Instantiate a Slim application
+ *
+ * This example instantiates a Slim application using
+ * its default settings. However, you will usually configure
+ * your Slim application now by passing an associative array
+ * of setting names and values into the application constructor.
+ */
+$app = new \Slim\Slim(array(
+    'templates.path' => '../templates',
+    'view' => new \Slim\Views\Twig(),
+    'debug' => true
+));
+
+$view = $app->view();
+$view->parserOptions = array(
+    'debug' => false
+    //'cache' => dirname(__FILE__) . '/cache',
+);
+
+$view->parserExtensions = array(
+    new \Slim\Views\TwigExtension()
+);
+
+/**
+ * Step 3: Define the Slim application routes
+ *
+ * Here we define several Slim application routes that respond
+ * to appropriate HTTP request methods. In this example, the second
+ * argument for `Slim::get`, `Slim::post`, `Slim::put`, `Slim::patch`, and `Slim::delete`
+ * is an anonymous function.
+ */
+
+// @TODO: replace with /update
+/*
+$app->get(
+    '/',
+    function () use ($app) {
+        $latestIdx = $app->latestIdx;
+        $app->render('update.html', array('title' => 'How Did I Get Here?',
+                                          'styleName' => 'bstation',
+                                          'currentIdx' => $latestIdx,
+                                          'previousIdx' => $latestIdx - 1,
+                                          'nextIdx' => $latestIdx + 1,
+                                          'leftLink' => 'log',
+                                          'leftLabel' => 'Log',
+                                          'rightLink' => 'news',
+                                          'rightLabel' => 'News'));
+    }
+);
+*/
+
+$app->get(
+    '/500',
+    function () use ($app) {
+        $app->render('500.html');
+    }
+);
+
+$app->get(
+    '/404',
+    function () use ($app) {
+        $app->render('404.html');
+    }
+);
+
+$app->get(
+    '/',
+    function () use ($app) {
+        $app->render('home.html', array('styleName' => 'bstation'));
+    }
+);
+
+$app->get(
+    '/add',
+    function () use ($app) {
+        $app->render('add.html');
+    }
+);
+
+/**
+ * Step 4: Run the Slim application
+ *
+ * This method should be called last. This executes the Slim application
+ * and returns the HTTP response to the HTTP client.
+ */
+$app->run();
